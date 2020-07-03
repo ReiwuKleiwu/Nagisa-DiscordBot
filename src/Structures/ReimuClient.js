4084a2b7967df0c2bb0928db57fb4aa79f3e955c
ReiwuKleiwu <xxstruckixx@gmail.com>
2020-07-03 11:38:53 +0200

    Added Command Handler

--- src/Structures/ReimuClient.js
+++ src/Structures/ReimuClient.js
@@ -1,55 +1,65 @@
 const {
-    Client
+	Client, Collection
 } = require('discord.js');
 
+const Util = require('./Util.js');
+
 module.exports = class ReimuClient extends Client {
-    constructor(options = {}) {
-        super({
-            disableMentions: 'everyone'
-        });
 
-        this.validate(options);
+	constructor(options = {}) {
+		super({
+			disableMentions: 'everyone'
+		});
 
-        this.once('ready', () => {
-            console.log(`Logged in as ${this.user.username}!`);
-        });
+		this.validate(options);
 
+		this.commands = new Collection();
 
-        this.on('message', async (message) => {
-            const mentionRegex = RegExp(`^<@!${this.user.id}>$`);
-            const mentionRegexPrefix = RegExp(`^<@!${this.user.id}>`);
+		this.aliases = new Collection();
 
-            if (!message.guild || message.author.bot) return;
-
-            if (message.content.match(mentionRegex)) message.channel.send(`My prefix for ${message.guild.name} is \`${this.prefix}\`.`);
-
-            const prefix = message.content.match(mentionRegexPrefix) ?
-                message.content.match(mentionRegexPrefix)[0] : this.prefix;
+		this.utils = new Util(this);
 
 
-            const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
+		this.once('ready', () => {
+			console.log(`Logged in as ${this.user.username}!`);
+		});
 
-            console.log(args);
 
-            if (cmd.toLowerCase() === 'hello') {
-                message.channel.send('Hello!');
-            }
-        });
-    }
+		this.on('message', async (message) => {
+			const mentionRegex = RegExp(`^<@!${this.user.id}>$`);
+			const mentionRegexPrefix = RegExp(`^<@!${this.user.id}>`);
 
-    validate(options) {
-        if (typeof options !== 'object') throw new TypeError('Options should be a type of Object.')
+			if (!message.guild || message.author.bot) return;
 
-        if (!options.token) throw new Error('You must pass the token for the client!');
-        this.token = options.token;
+			if (message.content.match(mentionRegex)) message.channel.send(`My prefix for ${message.guild.name} is \`${this.prefix}\`.`);
 
-        if (!options.prefix) throw new Error('You must pass a prefix for the client.');
-        if (typeof options.prefix !== 'string') throw new TypeError('Prefix should be a type of String.');
-        this.prefix = options.prefix;
+			const prefix = message.content.match(mentionRegexPrefix) ?
+				message.content.match(mentionRegexPrefix)[0] : this.prefix;
 
-    }
 
-    async login(token = this.token) {
-        super.login(token);
-    }
-}
+			const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
+
+			const command = this.commands.get(cmd.toLowerCase()) || this.commands.get(this.aliases.get(cmd.toLowerCase()));
+			if (command) {
+				command.run(message, args);
+			}
+		});
+	}
+
+	validate(options) {
+		if (typeof options !== 'object') throw new TypeError('Options should be a type of Object.');
+
+		if (!options.token) throw new Error('You must pass the token for the client!');
+		this.token = options.token;
+
+		if (!options.prefix) throw new Error('You must pass a prefix for the client.');
+		if (typeof options.prefix !== 'string') throw new TypeError('Prefix should be a type of String.');
+		this.prefix = options.prefix;
+	}
+
+	async start(token = this.token) {
+		this.utils.loadCommands();
+		super.login(token);
+	}
+
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const ReimuClient = require('./Structures/ReimuClient');
 const config = require('../config.json');
 
-const client = new ReimuClient(config); 
-client.login(); 
+const client = new ReimuClient(config);
+client.start();
