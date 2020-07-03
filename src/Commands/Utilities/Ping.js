a642d255a5bc0fefcaa9f02407e61de78c3c0db2
ReiwuKleiwu <xxstruckixx@gmail.com>
2020-07-03 12:10:27 +0200

    Added Ping and Uptime Command

---
+++ src/Commands/Utilities/Ping.js
@@ -0,0 +1,20 @@
+const Command = require('../../Structures/Command');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['latency']
+		});
+	}
+
+	async run(message) {
+		const msg = await message.channel.send('Pingig...');
+
+		const latency = msg.createdTimestamp - message.createdTimestamp;
+
+
+		msg.edit(`Bot Latency: \`${latency}ms\`, API Latency: \`${Math.round(this.client.ws.ping)}ms\``);
+	}
+
+};
